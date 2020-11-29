933e5808970c8ee275843879ed5ff2366e0c4eed
tayyb@Tayyab-pc <you@example.com>
2020-11-29 21:58:49 +0500

    Methods in java

---
+++ methodtester.java
@@ -0,0 +1,9 @@
+public class methodtester {
+  static void myMethod() {
+    System.out.println("Alhumdulilha almost donelast tak of assignment3!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+  }
+}
