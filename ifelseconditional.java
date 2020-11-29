a773298dd6f3c4f227d50728ca9b15910e03c9fc
tayyb@Tayyab-pc <you@example.com>
2020-11-29 21:54:04 +0500

    if-else statement

---
+++ ifelseconditional.java
@@ -0,0 +1,14 @@
+public class ifelseconditional {
+  public static void main(String[] args) {
+    int a = 20;
+    int b = 18;
+    if (a > b) {
+      System.out.println("a is greater than b");
+    }
+
+else {
+      System.out.println("b is greater than a");
+     }
+  
+  }
+}
