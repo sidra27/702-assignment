1ad163702d1dc42ba49880b28dcb9c3961a1ad23
tayyb@Tayyab-pc <you@example.com>
2020-11-29 21:49:17 +0500

    3rd assignment part 1

---
+++ idetester.java
@@ -0,0 +1,15 @@
+public class idetester{
+
+  public static void main(String[] args) {
+
+    int x, y, z;
+    
+    x = 5; 
+
+    y = 6; 
+
+    z = 50;
+    
+    System.out.println(x + y + z);
+  }
+}
