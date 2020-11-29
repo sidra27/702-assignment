e0db8e9454932bbd5da8da8227331dbd43801ac9
tayyb@Tayyab-pc <you@example.com>
2020-11-29 21:52:11 +0500

    if statement

---
+++ ifcomparision.java
@@ -0,0 +1,9 @@
+public class ifcomparision {
+  public static void main(String[] args) {
+    int a = 20;
+    int b = 18;
+    if (a > b) {
+      System.out.println("a is greater than b");
+    }  
+  }
+}
