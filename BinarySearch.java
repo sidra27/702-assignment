542ed7d6e784eb187b264cef3867f9721db9b78c
tayyb@Tayyab-pc <you@example.com>
2020-12-09 17:02:42 +0500

    binary search code assignment 4

---
+++ BinarySearch.java
@@ -0,0 +1,48 @@
+
+ 
+import java.util.Arrays;
+
+
+public class BinarySearch {
+
+    
+    private BinarySearch() { }
+
+    
+    public static int indexOf(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    
+    public static int rank(int key, int[] a) {
+        return indexOf(a, key);
+    }
+
+    
+    public static void main(String[] args) {
+
+        
+        In in = new In(args[0]);
+        int[] allowlist = in.readAllInts();
+
+   
+        Arrays.sort(allowlist);
+
+      
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (BinarySearch.indexOf(allowlist, key) == -1)
+                StdOut.println(key);
+        }
+    }
+}
+
